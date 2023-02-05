200105d5c135dc622afbba73fc800b9ebde53957
MobbySchiller <kb.burzynski@gmail.com>
2023-02-05 21:17:03 +0100

    style: Error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+const Error = () => {
+    return (
+        <div className='w-full bg-error text-white text-center py-2'>
+            Login failed, please check if you entered the correct data
+        </div>
+    )
+}
+
+export default Error
