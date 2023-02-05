4c4269d99072386e08cbc92e7652bfa957aa549a
MobbySchiller <kb.burzynski@gmail.com>
2023-02-05 21:17:21 +0100

    feat: add Login validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 
+export type DataValidate = {
+  email: boolean,
+  password: boolean
+}
+
 const App: FC = () => {
+  const [succeedLogged, setSucceedLogged] = useState<boolean>(false)
+
+  const [isDataValidate, setIsDataValidate] = useState<DataValidate>({
+    email: true,
+    password: true
+  })
+
   return (
     <div className='flex justify-center items-center w-full min-h-screen bg-gradient-to-br from-gradient-primary to-gradient-secondary'>
-      {/* <Login /> */}
-      {/* <Signup /> */}
-      {<Home />}
+      {succeedLogged
+        ?
+        <Home />
+        :
+        <Login validation={{ isDataValidate, setIsDataValidate }} />}
     </div>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,55 @@
-import { FC } from 'react'
+import React, { FC, useState } from 'react'
 import Container from '../components/Container'
 import Title from '../components/Title'
+import Error from '../components/Error'
+import { DataValidate } from '../App'
 
-const Login: FC = () => {
+const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+const MIN_PASSWORD_LENGTH = 6
+
+type LoginProps = {
+    validation: {
+        isDataValidate: DataValidate,
+        setIsDataValidate: React.Dispatch<React.SetStateAction<DataValidate>>
+    }
+}
+
+type LoginData = {
+    email: string,
+    password: string
+}
+
+const Login: FC<LoginProps> = ({ validation }) => {
+    const [loginData, setLoginData] = useState<LoginData>({
+        email: '',
+        password: ''
+    })
+
+    const { isDataValidate, setIsDataValidate } = validation
+
+    const handleSubmit = (event: React.SyntheticEvent) => {
+        event.preventDefault()
+        checkData()
+    }
+
+    const checkData = () => {
+        const { email, password } = loginData
+        const isEmailCorrect = Boolean(email.match(EMAIL_REGEX))
+        const isPasswordCorrect = Boolean(password.length >= MIN_PASSWORD_LENGTH)
+        setIsDataValidate({
+            email: isEmailCorrect,
+            password: isPasswordCorrect
+        })
+    }
+
     return (
         <Container>
             <Title />
-            <form className="flex flex-col px-8 pt-6 pb-2">
+            {/* <Error /> */}
+            <form
+                className="flex flex-col px-8 pt-6 pb-2"
+                onSubmit={handleSubmit}
+            >
                 <div className="mb-4">
                     <label
                         className="block text-primary text-sm font-bold mb-2"
@@ -17,8 +60,14 @@ const Login: FC = () => {
                         className="appearance-none border rounded w-full py-2 px-3 bg-input text-primary mb-1 leading-tight focus:outline-important"
                         id="email"
                         type="text"
-                        placeholder="Enter email" />
-                    <p className="text-error text-xs italic">Please enter an email.</p>
+                        placeholder="Enter email"
+                        value={loginData.email}
+                        onChange={(event) => setLoginData({ ...loginData, email: event.target.value })} />
+                    {!isDataValidate.email &&
+                        <p className="text-error text-xs italic">
+                            Invalid email.
+                        </p>
+                    }
                 </div>
                 <div className="mb-6">
                     <label
@@ -30,11 +79,20 @@ const Login: FC = () => {
                         className="appearance-none border rounded bg-input w-full py-2 px-3 text-primary mb-1 leading-tight focus:outline-important"
                         id="password"
                         type="password"
-                        placeholder="Enter password" />
-                    <p className="text-error text-xs italic">Please choose a password.</p>
+                        placeholder="Enter password"
+                        value={loginData.password}
+                        onChange={(event) => setLoginData({ ...loginData, password: event.target.value })}
+                    />
+                    {!isDataValidate.password &&
+                        <p className="text-error text-xs italic">
+                            Invalid password.
+                        </p>
+                    }
                 </div>
                 <button
-                    className="bg-important text-white font-bold py-2 px-4 rounded" type="button">
+                    className="bg-important text-white font-bold py-2 px-4 rounded"
+                    type="submit"
+                >
                     Sign up
                 </button>
             </form>
