652ddf0f11051d8c072ea1a770f9847ce23b1539
MobbySchiller <kb.burzynski@gmail.com>
2023-03-01 11:46:22 +0100

    style: add box-shadow to Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,9 +5,12 @@ type ChildrenProps = {
 }
 
 const Container: FC<ChildrenProps> = ({ children }) => (
-    <div className='w-11/12 max-w-md rounded-lg bg-white pt-4 pb-6'>
+    <div
+        className='w-11/12 max-w-md rounded-lg bg-white pt-4 pb-6'
+        style={{ boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px' }}
+    >
         {children}
-    </div>
+    </div >
 )
 
 export default Container
